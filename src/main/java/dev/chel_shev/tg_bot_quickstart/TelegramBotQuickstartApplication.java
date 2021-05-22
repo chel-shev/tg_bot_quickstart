0af6949d1cca2d386eb368d149078928bdda69e1
chel_shev <ixec2011@gmail.com>
2021-05-22 16:51:29 +0300

    init commit

---
+++ src/main/java/dev/chel_shev/tg_bot_quickstart/TelegramBotQuickstartApplication.java
@@ -0,0 +1,13 @@
+package dev.chel_shev.tg_bot_quickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotQuickstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotQuickstartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/chel_shev/telegram_bot_quickstart/TelegramBotQuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.chel_shev.telegram_bot_quickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotQuickstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
